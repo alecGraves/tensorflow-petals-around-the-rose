ce30033a420563c2bdab9396b87e6aa3b2cfe8de
shadySource <alec0061@hotmail.com>
2016-10-27 06:43:04 -0400

    brain_dump

--- petals_complex.py
+++ petals_complex.py
@@ -60,37 +60,71 @@ def get_data():
                 pass
         ins.append(tmp)
     dataset_file.close()
-    return np.array(ins), np.array(outs)
+    ins = np.array(ins)
+    outs = np.array(outs)
+    # Convert to better format for pur purposes
+    tmp = np.zeros((ins.shape[0], 6))
+    for i in range(ins.shape[0]):
+        for j in range(ins.shape[1]):
+            tmp[i][ins[i][j]-1] = tmp[i][ins[i][j]-1] + 0.2
+    ins = tmp
+    for i in range(len(outs)):
+        outs[i] = 1/(1+np.exp(-1*outs[i]/20.0))
+        if(i % 10000 == 0):
+            print(ins[i], outs[i])
+    return ins, outs
 
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(-0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+#get training data:
 inputs, answers = get_data()
+
 
 # Start TensorFlow part
 sess = tf.InteractiveSession()
 
-x = tf.placeholder(tf.float32, shape=[None, 5])
+x = tf.placeholder(tf.float32, shape=[None, 6])
 y_ = tf.placeholder(tf.float32, shape=[None, 1])
 
-W = tf.Variable(tf.zeros([5,1]))
-b = tf.Variable(tf.zeros([1]))
+W = weight_variable((6,1))
+b = bias_variable([1])
+
+y = tf.sigmoid(tf.matmul(x, W) + b)
+#y = tf.tanh(tf.matmul(x, W) + b)
+
+#cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+#cross_entropy = -tf.reduce_sum(y_*tf.log(y), reduction_indices=[1])
+cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(y, y_)
+
+train_step = tf.train.GradientDescentOptimizer(1e-4).minimize(cross_entropy)
 
 sess.run(tf.initialize_all_variables())
 
-y = tf.matmul(x,W) + b
-
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
-
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-for i in range(1000):
-    batchx, batchy = make_batch(inputs, answers)
+for i in range(100000):
+    batchx, batchy = make_batch(inputs, answers, 1000)
     train_step.run(feed_dict={x: batchx, y_:batchy})
+    if (i%1000 == 0):
+        print(sess.run(W, train_step.run(feed_dict={x: batchx, y_:batchy})))
 
 # Evaluation of the model:
 
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+correct_prediction = tf.greater(0.1, tf.abs(tf.sub((y), (y_))))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#accuracy = tf.cast(correct_prediction, tf.float32)
 
-batchx, batchy = make_batch(inputs, answers)
+batchx, batchy = make_batch(inputs, answers, 100)
+
+accuracyy = tf.cast(correct_prediction, tf.float32)
 
 print("Accuracy: ", sess.run(accuracy, feed_dict={x: batchx, y_: batchy}))
+print(sess.run(tf.abs(tf.sub((y), (y_))),feed_dict={x: batchx, y_: batchy}))
+
